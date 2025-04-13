6307de99d9ad1616c98a8db231e7782a34e2219a
onixtsm <alexander@sautkin.com>
2025-04-13 13:10:00 +0200

    Unet

--- nn.py
+++ nn.py
@@ -47,8 +47,65 @@ def plot_comparison(input_img, label_img, prediction_img):
     plt.show()
 
 
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False), # k_size=3
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNET(nn.Module):
+    def __init__(self, in_channels=3, out_channels=1, features = [64, 128, 256, 512]):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Down part of UNET
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2) # check what this does
+            )
+            self.ups.append(DoubleConv(feature * 2, feature))
+
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for id in range(0, len(self.ups), 2):
+            x = self.ups[id](x)
+            skip = skip_connections[id//2]
+            concat_skip = torch.cat((skip, x), dim=1)
+            x = self.ups[id+1](concat_skip)
+
+        return self.final_conv(x)
+
+
 class ViscosityNet2(nn.Module):
-    def __init__(self, n, drop=0.5, pool=2, k_size=3, device="cpu") -> None:
+    def __init__(self, n, drop=0.5, pool=2, k_size=3) -> None:
         b = True
         super().__init__()
         layer_count = int(np.ceil(63.0 / k_size))
@@ -148,11 +205,11 @@ def homogenise_labels(labels: np.array, params: pd.DataFrame):
 
 
 def mean_loss(pred: torch.Tensor, truth: torch.Tensor, eps: float = 1e-9):
-    a = 0
+    alpha = 0
     assert isinstance(eps, float)
     rel = torch.abs((truth - pred) / (truth + eps))
     abs = torch.abs(truth - pred)
-    error = a * rel + (1 - a) * abs
+    error = alpha * rel + (1 - alpha) * abs
     return error.mean(dim=(1, 2)).mean()
 
 
@@ -206,7 +263,7 @@ def train_model(
                 loss_dict["test"].append(test_loss.item())
                 if test_loss.item() < best_loss:
                     best_model = copy.deepcopy(model)
-                    best_loss = copy.copy(test_loss.item())
+                    best_losxs = copy.copy(test_loss.item())
                     e = copy.copy(epoch)
 
             if (epoch + 1) % print_every == 0:
@@ -272,8 +329,9 @@ def main() -> None:
     model = None
     old_loss_dict = {"train": [], "test": []}
     if settings.model is None:
-        model = ViscosityNet2(n=settings.neurons, k_size=settings.kernel_size,
-                              ).to(settings.device)
+        # model = ViscosityNet2(n=settings.neurons, k_size=settings.kernel_size,
+        #                       ).to(settings.device)
+        model = UNET(in_channels=inputs_h.shape[1]).to(settings.device)
     else:
         l = torch.load(settings.model)
         model = l['model']
