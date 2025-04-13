98b99e4af4de040669e6bda32a051489b4a329a1
onixtsm <alexander@sautkin.com>
2025-04-13 21:34:00 +0200

    Fix of indent

--- nn.py
+++ nn.py
@@ -51,7 +51,7 @@ class DoubleConv(nn.Module):
   def __init__(self, in_channels, out_channels):
     super(DoubleConv, self).__init__()
     self.conv = nn.Sequential(
-      nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False), # k_size=3
+      nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),  # k_size=3
       nn.BatchNorm2d(out_channels),
       nn.ReLU(inplace=True),
       nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
@@ -64,7 +64,7 @@ class DoubleConv(nn.Module):
 
 
 class UNET(nn.Module):
-  def __init__(self, in_channels=3, out_channels=1, features = [4, 8, 16, 32]):
+  def __init__(self, in_channels=3, out_channels=1, features=[4, 8, 16, 32]):
     super(UNET, self).__init__()
     self.ups = nn.ModuleList()
     self.downs = nn.ModuleList()
@@ -77,7 +77,8 @@ class UNET(nn.Module):
 
     for feature in reversed(features):
       self.ups.append(
-        nn.ConvTranspose2d(feature * 2, feature, kernel_size=2, stride=2) # check what this does
+        nn.ConvTranspose2d(feature * 2, feature, kernel_size=2,
+                           stride=2)  # check what this does
       )
       self.ups.append(DoubleConv(feature * 2, feature))
 
@@ -186,7 +187,7 @@ def split_data(data, percent=80):
 
 def homogenise_params(params: pd.DataFrame) -> pd.DataFrame:
   params["q"] = torch.square(params["delta_A"]) * \
-    params["delta_p"] / (params["visc"] * params["L"]) 
+    params["delta_p"] / (params["visc"] * params["L"])
   return params
 
 
@@ -268,7 +269,7 @@ def train_model(
 
             if (epoch + 1) % print_every == 0:
                 print(
-                    f"{epoch+1: <7}  {loss_dict['train'][-1]: <14.6e}  {loss_dict['test'][-1]: <13.6e}"
+                    f"{epoch+1: <7}  {loss_dict['train'][-1]                        : <14.6e}  {loss_dict['test'][-1]: <13.6e}"
                 )
     except KeyboardInterrupt:
         pass
